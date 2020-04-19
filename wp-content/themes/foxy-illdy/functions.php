b07a20ae5cc562cc0190478df836822b184af662
Reinier van den Assum <reinier@fox-creation.nl>
2020-04-19 19:10:09 +0200

    2020-04-19: Minor updates
    * Ensuring Post-meta widget only shows for single posts pages (not on the generic blog page)
    * Updating front-page-latest-news.php to show the Excerpt (when existing) else show the 40 characters of the post

--- wp-content/themes/foxy-illdy/functions.php
+++ wp-content/themes/foxy-illdy/functions.php
@@ -22,26 +22,30 @@ class postmeta_widget extends WP_Widget{
                             );
     }
 
-    // Creating widget front-end
+    /**
+	 * When widget is loaded on single post page, show post meta details
+	 */
     public function widget( $args, $instance ){
-		$categoriesList = get_the_category_list( esc_html__( ', ', 'illdy' ) );
-		$tagsList = get_the_tag_list( '', ' | ', '' );
-		$number_comments = get_comments_number();
-		
-        echo $args['before_widget'];
-        $output = '<div class="blog-post-meta">';
-		$output .= '<div class="post-meta-time"><i class="fa fa-calendar"></i><time datetime="' . sprintf( '%s-%s-%s', get_the_date( 'Y' ), get_the_date( 'm' ), get_the_date( 'd' ) ) . '">' . get_the_date() . '</time></div>';
-		$output .= '<div class="post-meta-categories"><i class="fa fa-folder-o" aria-hidden="true"></i>' . $categoriesList . '</div>';
-		$output .= '<div class="post-meta-tags"><i class="fa fa-tags" aria-hidden="true"></i>' . $tagsList . '</div>';
-		$output .= ( ( comments_open() )
-					? ( ( 0 == $number_comments )
-						? sprintf( '<div class="post-meta-comments"><i class="fa fa-comment-o"></i>' . __( 'No comments', 'illdy' ) . '</div>' )
-						: ( ( $number_comments > 1 )
-							? sprintf( '<div class="post-meta-comments"><i class="fa fa-comment-o"></i><a class="meta-comments" href="%s" title="%s ' . __( 'comments', 'illdy' ) . '">%s ' . __( 'comments', 'illdy' ) . '</a></div>', get_comments_link(), $number_comments, $number_comments )
-							: sprintf( '<div class="post-meta-comments"><i class="fa fa-comment-o"></i><a class="meta-comments" href="%s" title="' . __( '1 comment', 'illdy' ) . '">' . __( '1 comment', 'illdy' ) . '</a></div>', get_comments_link() ) ) )
-					: sprintf( '<div class="post-meta-comments"><i class="fa fa-comment-o"></i>' . __( 'Comments are off for this post', 'illdy' ) . '</div>' ) );
-		$output .= '</div><!--/.blog-post-meta-->';
-        echo $output;
-        echo $args['after_widget'];
+		if( is_single() ){
+			$categoriesList = get_the_category_list( esc_html__( ', ', 'illdy' ) );
+			$tagsList = get_the_tag_list( '', ' | ', '' );
+			$number_comments = get_comments_number();
+
+			echo $args['before_widget'];
+			$output = '<div class="blog-post-meta">';
+			$output .= '<div class="post-meta-time"><i class="fa fa-calendar"></i><time datetime="' . sprintf( '%s-%s-%s', get_the_date( 'Y' ), get_the_date( 'm' ), get_the_date( 'd' ) ) . '">' . get_the_date() . '</time></div>';
+			$output .= '<div class="post-meta-categories"><i class="fa fa-folder-o" aria-hidden="true"></i>' . $categoriesList . '</div>';
+			$output .= '<div class="post-meta-tags"><i class="fa fa-tags" aria-hidden="true"></i>' . $tagsList . '</div>';
+			$output .= ( ( comments_open() )
+						? ( ( 0 == $number_comments )
+							? sprintf( '<div class="post-meta-comments"><i class="fa fa-comment-o"></i>' . __( 'No comments', 'illdy' ) . '</div>' )
+							: ( ( $number_comments > 1 )
+								? sprintf( '<div class="post-meta-comments"><i class="fa fa-comment-o"></i><a class="meta-comments" href="%s" title="%s ' . __( 'comments', 'illdy' ) . '">%s ' . __( 'comments', 'illdy' ) . '</a></div>', get_comments_link(), $number_comments, $number_comments )
+								: sprintf( '<div class="post-meta-comments"><i class="fa fa-comment-o"></i><a class="meta-comments" href="%s" title="' . __( '1 comment', 'illdy' ) . '">' . __( '1 comment', 'illdy' ) . '</a></div>', get_comments_link() ) ) )
+						: sprintf( '<div class="post-meta-comments"><i class="fa fa-comment-o"></i>' . __( 'Comments are off for this post', 'illdy' ) . '</div>' ) );
+			$output .= '</div><!--/.blog-post-meta-->';
+			echo $output;
+			echo $args['after_widget'];
+		}
     }
 }
