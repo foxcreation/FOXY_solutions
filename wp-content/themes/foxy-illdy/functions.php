8919d4e802ef8478124af999e59d2954a4800ce8
Reinier van den Assum <reinier@fox-creation.nl>
2020-05-09 17:59:07 +0200

    2020-05-09: Updating Blog-Post-Meta to show on inline-archive post-block as well
    * Updated class/object a bit to reduce duplicate code
    * Update to style.css to have styling show nicely

--- wp-content/themes/foxy-illdy/functions.php
+++ wp-content/themes/foxy-illdy/functions.php
@@ -27,25 +27,43 @@ class postmeta_widget extends WP_Widget{
 	 * When widget is loaded on single post page, show post meta details
 	 */
     public function widget( $args, $instance ){
-		if( is_single() ){
-			$categoriesList = get_the_category_list( esc_html__( ', ', 'illdy' ) );
-			$tagsList = get_the_tag_list( '', ' | ', '' );
-			$number_comments = get_comments_number();
+        $categoriesList = get_the_category_list( esc_html__( ', ', 'illdy' ) );
+        $tagsList = get_the_tag_list( '', ' | ', '' );
 
+		if( is_single() ){
 			echo $args['before_widget'];
 			$output = '<div class="blog-post-meta">';
-			$output .= '<div class="post-meta-time"><i class="fa fa-calendar"></i><time datetime="' . sprintf( '%s-%s-%s', get_the_date( 'Y' ), get_the_date( 'm' ), get_the_date( 'd' ) ) . '">' . get_the_date() . '</time></div>';
+			$output .= '<div class="post-meta-time"><i class="fa fa-calendar"></i>'. $this->getTimeText() .'</div>';
 			$output .= '<div class="post-meta-categories"><i class="fa fa-folder-o" aria-hidden="true"></i>' . ( ( empty( $categoriesList ) ) ? '-' : $categoriesList ) . '</div>';
             $output .= '<div class="post-meta-tags"><i class="fa fa-tags" aria-hidden="true"></i>' . ( ( empty( $tagsList ) ) ? '-' : $tagsList ). '</div>';
-			$output .= ( ( comments_open() )
-						? ( ( 0 == $number_comments )
-							? sprintf( '<div class="post-meta-comments"><i class="fa fa-comment-o"></i>' . __( 'No comments', 'illdy' ) . '</div>' )
-							: ( ( $number_comments > 1 )
-								? sprintf( '<div class="post-meta-comments"><i class="fa fa-comment-o"></i><a class="meta-comments" href="%s" title="%s ' . __( 'comments', 'illdy' ) . '">%s ' . __( 'comments', 'illdy' ) . '</a></div>', get_comments_link(), $number_comments, $number_comments )
-								: sprintf( '<div class="post-meta-comments"><i class="fa fa-comment-o"></i><a class="meta-comments" href="%s" title="' . __( '1 comment', 'illdy' ) . '">' . __( '1 comment', 'illdy' ) . '</a></div>', get_comments_link() ) ) )
-						: sprintf( '<div class="post-meta-comments"><i class="fa fa-comment-o"></i>' . __( 'Comments are off for this post', 'illdy' ) . '</div>' ) );
-			$output .= '</div><!--/.blog-post-meta-->';
+			$output .= '<div class="post-meta-comments"><i class="fa fa-comment-o"></i>'. $this->getCommentsText() .'</div>';
+			$output .='</div><!--/.blog-post-meta-->';
 			echo $output . $args[ 'after_widget' ];
+		} else if( is_archive() ){
+            echo $args['before_widget'];
+            $output = '<div class="blog-post-meta">';
+            $output .= '<span class="post-meta-time"><i class="fa fa-calendar"></i>'. $this->getTimeText() .'</span>';
+            $output .= '<span class="post-meta-categories"><i class="fa fa-folder-o" aria-hidden="true"></i>' . ( ( empty( $categoriesList ) ) ? '-' : $categoriesList ) . '</span>';
+            $output .= '<span class="post-meta-comments"> <i class="fa fa-comment-o"></i>'. get_comments_number() .'</span>';
+            $output .= '<br />';
+            $output .= '<span class="post-meta-tags"><i class="fa fa-tags" aria-hidden="true"></i>' . ( ( empty( $tagsList ) ) ? '-' : $tagsList ). '</span>';
+            $output .= '</div><!--/.blog-post-meta-->';
+            echo $output . $args[ 'after_widget' ];
 		}
+    }
+
+    public function getTimeText(){
+        return '<time datetime="' . sprintf( '%s-%s-%s', get_the_date( 'Y' ), get_the_date( 'm' ), get_the_date( 'd' ) ) . '">' . get_the_date() . '</time>';
+    }
+
+    public function getCommentsText(){
+        $number_comments = get_comments_number();
+        return ( ( comments_open() )
+                ? ( ( 0 == $number_comments )
+                    ? __( 'No comments', 'illdy' )
+                    : ( ( $number_comments > 1 )
+                        ? sprintf( '<a class="meta-comments" href="%s" title="%s ' . __( 'comments', 'illdy' ) . '">%s ' . __( 'comments', 'illdy' ) . '</a>', get_comments_link(), $number_comments, $number_comments )
+                        : sprintf( '<a class="meta-comments" href="%s" title="' . __( '1 comment', 'illdy' ) . '">' . __( '1 comment', 'illdy' ) . '</a>', get_comments_link() ) ) )
+                : __( 'Comments are off for this post', 'illdy' ) );
     }
 }
